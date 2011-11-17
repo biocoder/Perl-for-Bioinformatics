ed1c0d245e6e18f9d989ee7ce554a4cdc78c68a8
Bio Coder <k.kranti.neu@gmail.com>
2011-11-17 17:53:34 -0500

    Example Code to demonstrate Perl Operators

---
+++ perl_operators.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+
+# BioCoder
+# 11/17/2011
+# This script contains examples of different perl operations
+
+use strict;
+use warnings;
+
+my $x = 27;
+my $y = 10;
+my $string_1 = 'My name is';
+my $string_2 = 'BioCoder';
+
+# Add
+my $add = $x + $y;
+
+# Substract
+my $substract = $x - $y;
+
+# Multiply
+my $multiply = $x * $y;
+
+# Divide
+my $divide = $x / $y;
+
+# Power
+my $exp = $x ** $y;
+
+# Reminder
+my $rem = $x % $y;
+
+# Print above results
+
+print "\nSum: $x + $y = $add\n";
+print "Substraction: $x - $y = $substract\n";
+print "Multiplication: $x x $y = $multiply\n";
+print "Division: $x / $y = $divide\n";
+print "Reminder: Reminder of $x divided by $y = $rem\n\n";
+
+# Increment and Decrement Operators
+
+
