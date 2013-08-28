7cd5c9c3f87a39c470554fda5d1520463895f8cf
biocoder <k.kranti.neu@gmail.com>
2013-08-28 11:32:07 -0500

    Adding IO::Routine module to repo

---
+++ IO-Routine/lib/IO/Routine.pm
@@ -0,0 +1,655 @@
+package IO::Routine;
+
+use 5.14.2;
+use strict;
+use warnings FATAL => 'all';
+use Carp;
+use Pod::Usage;
+use Module::Load;
+use Exporter;
+
+=head1 NAME
+
+=over 3
+
+IO::Routine - An attempt to provide a solution to avoid routine IO chores.
+
+=back
+
+=head1 VERSION
+
+=over 3
+
+Version 0.06
+
+=back
+
+=cut
+
+our $VERSION = '0.06';
+
+
+=head1 SYNOPSIS
+
+=over 3
+
+To avoid some of the repetetive stuff, this module:
+
+=back
+
+=over 4
+
+=item * Checks file / directory paths.
+
+=item * Verifies command line options used with Getopt module.
+
+=item * Executes system level commands, while printing message to STDOUT.
+
+=item * Checks for minimum version requirement for GNU Core Utils.
+
+=item * Prints TL;DR version of required modules list.
+
+=item * Prints SVN version control information (If requested).
+
+=item * Gets current memory usage of the program ( in GB(s) for Linux ).
+
+=back
+
+=head1 USAGE
+
+=over 4
+
+    use IO::Routine qw[check_and_load_modules];
+
+    my $io = IO::Routine->new();
+    $io->check_and_load_modules(['Getopt::Long']);
+
+    my $is_valid_option = GetOptions('help|?' => \$help,
+                                     'quiet' => \$quiet,
+                                     'output=s' => \$output,
+                                     'file1=s' => \$file1,
+                                     'file2=s' => \$file2
+                                     );
+
+    $io->verify_options($is_valid_option);
+    $io->verify_files([$file2, $file2], ['file1 msg', 'file2 msg'])
+
+    my $output = $io->validate_create_path($output, 'create');
+    my $output = $io->validate_create_path($output, 'do not create');
+
+    $io->execute_system_command(0,
+                                "\nChecking the version requirement for system level commands ...\n",
+                                $quiet);
+    my $unix_date = $io->execute_get_sys_command_output("/bin/date",
+                                                        "\nGetting system date ... \n",
+                                                        $quiet);
+    $io->check_sys_level_cmds(['grep', 'ls', 'sort', 'basename'],
+                              ['2.10', '8.13', '8.13', '8.13']);
+
+    my ($v_mem, $r_mem) = $io->get_mem_usage();
+
+=back
+
+=head1 SUBROUTINES/METHODS
+
+=over 4
+
+=item check_and_load_modules()
+
+=back
+
+=over 5
+
+Checks and loads modules.
+
+=back
+
+=over 4
+
+=item verify_options()
+
+=back
+
+=over 5
+
+Verifies options declared with Getopt.
+
+=back
+
+=over 4
+
+=item verify_files()
+
+=back
+
+=over 5
+
+Verifies the existence of input files.
+
+=back
+
+=over 4
+
+=item is_dir_empty()
+
+=back
+
+=over 5
+
+Checks for existence of directory and whether or not it holds some files.
+
+=back
+
+=over 4
+
+=item validate_create_path()
+
+=back
+
+=over 5
+
+Validates and creates output path, if asked.
+
+=back
+
+=over 4
+
+=item execute_system_command()
+
+=back
+
+=over 5
+
+Method to enable / disable verbosity of various script actions, while executing system level commands.
+
+=back
+
+=over 4
+
+=item execute_get_sys_cmd_output()
+
+=back
+
+=over 5
+
+Execute system level command and capture everything from STDERR and STDOUT.
+
+=back
+
+=over 4
+
+=item check_sys_level_commands()
+
+=back
+
+=over 5
+
+Checks the minimum version requirement of GNU Core Utils.
+
+=back
+
+=over 4
+
+=item error()
+
+=back
+
+=over 5
+
+Prints error with msg to STDERR and quits.
+
+=back
+
+=over 4
+
+=item warning()
+
+=back
+
+=over 5
+
+Prints warning message to STDOUT and continues.
+
+=back
+
+=over 4
+
+=item open_file()
+
+=back
+
+=over 5
+
+Opens file in requested mode and returns file handle.
+
+=back
+
+=over 4
+
+=item this_script_info()
+
+=back
+
+=over 5
+
+Prints Program's name, version control information (Supported for only SVN).
+
+=back
+
+=over 4
+
+=item get_mem_usage()
+
+=back
+
+=over 5
+
+Get memory usage on LINUX systems ( in GB(s) ) based on PID from /proc/PID and using vmmap utility for OS X systems.  Useful in data structure loops. You need to specify $|++; to flush buffer.
+
+=back
+
+=head1 AUTHOR
+
+=over 3
+
+Kranti Konganti, E<lt>konganti@tamu.eduE<gt>.
+
+=back
+
+=head1 BUGS
+
+=over 3
+
+Please report any bugs or feature requests to C<bug-io-routine at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=IO-Routine>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+=back
+
+=head1 SUPPORT
+
+=over 3
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc IO::Routine
+
+
+You can also look for information at:
+
+=back
+
+=over 5
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=IO-Routine>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/IO-Routine>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/IO-Routine>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/IO-Routine/>
+
+=back
+
+=head1 LICENSE AND COPYRIGHT
+
+=over 3
+
+Copyright 2012 Kranti Konganti.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the the Artistic License (2.0). You may obtain a
+copy of the full license at:
+
+L<http://www.perlfoundation.org/artistic_license_2_0>
+
+Any use, modification, and distribution of the Standard or Modified
+Versions is governed by this Artistic License. By using, modifying or
+distributing the Package, you accept this license. Do not use, modify,
+or distribute the Package, if you do not accept this license.
+
+If your Modified Version has been derived from a Modified Version made
+by someone other than you, you are nevertheless required to ensure that
+your Modified Version complies with the requirements of this license.
+
+This license does not grant you the right to use any trademark, service
+mark, tradename, or logo of the Copyright Holder.
+
+This license includes the non-exclusive, worldwide, free-of-charge
+patent license to make, have made, use, offer to sell, sell, import and
+otherwise transfer the Package with respect to any patent claims
+licensable by the Copyright Holder that are necessarily infringed by the
+Package. If you institute patent litigation (including a cross-claim or
+counterclaim) against any party alleging that the Package constitutes
+direct or contributory patent infringement, then this Artistic License
+to you shall terminate on the date that such litigation is filed.
+
+Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
+AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
+THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
+YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
+CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
+CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=back
+
+=cut
+
+BEGIN {
+  use vars qw[@ISA @EXPORT_OK];
+  @ISA = qw[Exporter];
+  @EXPORT_OK = qw[check_and_load_modules];
+}
+
+# IO::Routine Constructor
+
+sub new {
+    my $class = shift;
+    my $self = {};
+    bless $self, $class;
+    return $self;
+}
+
+
+# Check and load modules.
+
+sub check_and_load_modules {
+    my $self = shift;
+    my $module_list = shift;
+    my $is_module_loadable = 1;
+    my $req_modules = '';
+
+    foreach my $module (@$module_list) {
+      eval { load $module };
+      $is_module_loadable = 0,
+        $req_modules .= "$module, "
+	  if ( $@ );
+    }
+    $req_modules =~ s/\,\s+$//;
+
+    confess error($self,
+		  "Cannot load requested module(s):\n$req_modules\n\nMake sure that the Perl modules are in your path.\n\nHint: Use PERL5LIB environment variable.\n")
+        if (!$is_module_loadable);
+
+    return 1;
+}
+
+
+# Check if all options entered by user are valid with Getopt.
+
+sub verify_options {
+    my $self = shift;
+    my $valid_options = shift;
+    my $help = shift;
+
+    if (!$valid_options) {
+        pod2usage(-exitval => 2,
+		  -verbose => 2,
+                  -msg => "\nSee $0 -h for options.\n");
+    }
+
+    if ($help) {
+        pod2usage(-exitval => 1,
+                  -sections => "OPTIONS",
+                  -msg => "\n");
+    }
+    return $self;
+}
+
+
+# Check the existence of files.
+
+sub verify_files {
+    my $self = shift;
+    my $files = shift;
+    my $what_files = shift;
+    my $file_no = 0;
+
+    foreach my $file (@$files) {
+
+        confess error($self, "@$what_files[$file_no] file not specified: $!")
+            if (!defined $file);
+
+        confess error($self, "@$what_files[$file_no] file ( $file ) does not exist: $!")
+            if (!-e $file);
+
+        confess error($self, "@$what_files[$file_no] file ($file) is empty: $!")
+            if (-s $file == 0);
+
+        $file_no++;
+    }
+    return $self;
+}
+
+# Check if the directory is empty
+
+sub is_dir_empty {
+    my $self = shift;
+    my $dirname = shift;
+
+    confess error("Directory $dirname does not exist")
+        if (!-d $dirname);
+
+    opendir (my $dh, $dirname) ||
+        confess error($self, "$dirname not a directory: $!");
+
+    my $there_are_files = 0;
+
+    while ( defined (my $file = readdir $dh) ) {
+        my $path_to_file = $dirname . $file;
+
+        if ($file eq '.' || $file eq '..') {
+            next;
+        }
+        elsif (-f $path_to_file) { # if atleast 1 file is present
+            $there_are_files++;
+            last;
+        }
+    }
+
+    close $dh;
+
+    if ($there_are_files) {
+        return 0;
+    }
+    else {
+        return 1;
+    }
+}
+
+# Check if output path is mentioned, else create output path, if asked with 'create' message.
+
+sub validate_create_path {
+    my $self = shift;
+    my $path = shift;
+    my $create_dir = shift;
+    my $msg = shift;
+
+    confess error ($self, "$msg path not defined or entered!")
+        if (!defined $path || $path eq '');
+
+    if ($create_dir eq 'create') {
+        if (defined $path) {
+            execute_system_command($self,
+                                   "mkdir -p $path",
+                                   "\nAttempting to create $path ...\n\n",
+                                   )
+                if (defined ($path) && !-d $path);
+        }
+        else {
+            $path = $ENV{'PWD'};
+        }
+    }
+
+    confess error($self, "Path ( $path ) does not exist: $!")
+        if (!-d $path);
+
+    $path .= '/'
+        if ($path !~ m/\/$/);
+
+    return $path;
+}
+
+# Subroutine to execute system command with verbosity.
+
+sub execute_system_command {
+    my $self = shift;
+    my $command = shift;
+    my $print_msg = shift;
+    my $quiet = shift;
+
+    if (!$quiet) {
+       print $print_msg if ($print_msg);
+       system ("$command") if ($command);
+    }
+    elsif ($quiet) {
+        system ("$command 1>/dev/null 2>/dev/null") if ($command);
+    }
+    return $self;
+}
+
+# Subroutine to execute and return system command's output
+
+sub execute_get_sys_cmd_output {
+    my $self = shift;
+    my $command = shift;
+    my $print_msg = shift;
+    my $quiet = shift;
+
+    if (!$quiet) {
+       print $print_msg if ($print_msg);
+     }
+
+    my $cmd_out = qx($command 2>&1);
+    $cmd_out = 'Could not capture stream from either STDERR or STDOUT' if (!$cmd_out || $cmd_out eq '');
+
+    return $cmd_out;
+}
+
+# Shell msg that differentiates log from error
+
+sub error {
+    my $self = shift;
+    my $msg = shift;
+    print STDERR "\nERROR!\n------\n$msg\n\n";
+    pod2usage(-exitval => 2,
+	      -verbose => 2);
+    return $self;
+}
+
+
+# Shell msg for warning
+
+sub warning {
+    my $self = shift;
+    my $msg = shift;
+    print STDOUT "\nWARNING!\n--------\n$msg\n\n";
+    return $self;
+}
+
+# Subroutine to open files and return file handle
+
+sub open_file {
+    my $self = shift;
+    my $mode = shift;
+    my $file = shift;
+    my $tainted_filename;
+
+    if ($file =~ m/(.+?)\z/i) {
+      $tainted_filename = $1;
+    }
+
+    open (my $file_handle, $mode, $tainted_filename) ||
+        confess error($self, "Cannot open ( $file ) in mode ( $mode ): $!");
+    return $file_handle;
+ }
+
+
+# Subroutine to print SCRIPT Version. Print this script's info
+
+sub this_script_info {
+    my ($self, $PRGNAME, $VERSION, $AUTHORFULLNAME, $CHANGEDBY, $LASTCHANGEDDATE) = @_;
+    print "\n", '@ ', '*' x 78, ' @', "\n";
+    print "    Program Name       :  " , $PRGNAME, "\n";
+    print "    Version            :  $VERSION\n" if ($VERSION);
+    print "    Author             :  $AUTHORFULLNAME\n" if ($AUTHORFULLNAME);
+    print "    Last Changed By    : $CHANGEDBY\n" if ($CHANGEDBY);
+    print "    Last Changed Date  : $LASTCHANGEDDATE\n";
+    print '@ ', '*' x 78, ' @', "\n\n";
+    return $self;
+}
+
+# Subroutine to check the least required version of system command.
+
+sub check_sys_level_cmds {
+    my $self = shift;
+    my $cmds = shift;
+    my $versions = shift;
+    my ($curr_version_unf, $req_version);
+    for (my $i=0; $i<scalar(@$cmds); $i++) {
+        my $cmd_version_out = `@$cmds[$i] --version`;
+        $req_version = @$versions[$i];
+        $req_version =~ s/\.//g;
+        $req_version =~ s/0//g;
+        if ($cmd_version_out =~ m/^@$cmds[$i].*?(\d+[\.\d]*)/ ||
+            $cmd_version_out =~ m/^.*?(\d+[\.\d]*)/) {
+            my $curr_version = $curr_version_unf = $1;
+            $curr_version =~ s/\.//g;
+            $curr_version =~ s/0//g;
+            if ($curr_version < $req_version) {
+                confess error($self,
+                              "At least Version @$versions[$i] required for system level command: @$cmds[$i]\nCurrent Version: $curr_version_unf\n");
+            }
+        }
+    }
+    return $self;
+}
+
+# Subroutine to strip leading and trailing white spaces from a line
+
+sub strip_leading_and_trailing_spaces {
+    my $self = shift;
+    my $line = shift;
+    $line =~ s/^\s+//;
+    $line =~ s/\s+$//;
+    return $line;
+}
+
+# Subroutine returns memory stats based on /proc/ on Linux machines
+
+sub get_mem_usage {
+    my $self = shift;
+    chomp(my $host_machine = `uname`);
+    if ($host_machine =~ m/darwin/i) {
+        chomp(my $malloc =`vmmap -resident $$ | grep -i 'see malloc zone table below'`);
+        my @mem_details = split/\s+/, $malloc;
+        return ('Unable to get vmem', 'Unable to get rmem') if (!$mem_details[1] || !$mem_details[2]);
+        return ($mem_details[1], $mem_details[2]);
+    }
+    elsif ($host_machine =~ m/linux/i) {
+        return ('Unable to get vmem', 'Unable to get rmem') if (!-e "/proc/$$/status");
+        my $curr_v_mem = `grep VmPeak /proc/$$/status`;
+        my $curr_r_mem = `grep VmSize /proc/$$/status`;
+        my ($curr_v_mem_usage) = $curr_v_mem =~ m/(\d+)/;
+        my ($curr_r_mem_usage) = $curr_r_mem =~ m/(\d+)/;
+        $curr_v_mem_usage = ( ($curr_v_mem_usage / 1024) / 1024 );
+        $curr_r_mem_usage = ( ($curr_r_mem_usage / 1024) / 1024 );
+        $curr_v_mem_usage = floor($curr_v_mem_usage) . 'GB';
+        $curr_r_mem_usage = floor($curr_r_mem_usage) . 'GB';
+        return ('Unable to get vmem', 'Unable to get rmem') if (!$curr_v_mem_usage || !$curr_r_mem_usage);
+        return ($curr_v_mem_usage, $curr_r_mem_usage);
+    }
+}
+
+
+1; # End of IO::Routine
---
+++ IO-Routine/t/01_IORoutine_methods.t
@@ -0,0 +1,101 @@
+#!perl -T
+
+use 5.14.2;
+use strict;
+use warnings FATAL => 'all';
+use Test::More tests => 16;
+
+my ($pathTest, $errorTest, $openFileTest, $tmpLoc) = 0;
+$|=0;
+
+diag("Tests begin ...");
+BEGIN {
+    use_ok( 'IO::Routine' ) || print "Bail out! Cannot load IO::Routine for testing!\n";
+}
+
+diag( "Testing IO::Routine $IO::Routine::VERSION, Perl $], $^X" );
+
+my $io = IO::Routine->new();
+diag( "Testing constructor new()" );
+ok( defined $io, 'IO::Routine->new() returns an object.');
+
+diag( "Testing for blessed object" );
+ok( $io->isa( 'IO::Routine' ), 'IO::Routine object verified.' );
+
+diag( "Testing method check_and_load_modules()" );
+ok( defined( $io->check_and_load_modules( ['File::Basename', 'Pod::Usage']) ), 'check_and_load_modules() loads core / optional modules.');
+
+diag( "Testing method verify_options()" );
+ok( defined( $io->verify_options( "1", "0" ) ), 'verify_options() - Requires Pod::Usage when using IO::Routine.');
+
+SKIP: {
+  skip(' ... Skipping methods: verify_input_files() ... Unable to test without providing UNIX paths.', 1)
+    if (!$pathTest);
+  ok( defined( $io->verify_input_files([''], [''])) );
+}
+
+diag( "Testing method is_dir_empty()" );
+ok( defined( $io->is_dir_empty("/tmp") ), 'is_dir_empty() checks for empty directory as intended.');
+
+SKIP: {
+  skip(' ... Skipping methods: validate_create_path() ... Unable to test without providing UNIX paths.', 1)
+    if (!$pathTest);
+  ok( defined( $io->validate_create_path('', '', '')) );
+}
+
+SKIP: {
+  skip(' ... Skipping methods: error() ... since error() prints fatal errors to STDERR', 1)
+    if (!$errorTest);
+  ok( $io->error( "Testing fatal errors ... " ) , 'error() - Is loadable and since fatal errors are printed to STDERR');
+}
+
+SKIP: {
+  skip(' ... Skipping methods: check_sys_level_cmds() ... since fatal errors are printed to STDERR', 1)
+    if (!$errorTest);
+  ok( $io->check_sys_level_cmds( ['grep'], ['2.10'] ) , 'check_sys_level_cmds() - Is loadable and since prints fatal errors to STDERR');
+}
+
+SKIP: {
+  skip(' ... Skipping methods: get_mem_usage() ... since it uses system level command grep', 1)
+    if (!$errorTest);
+  ok( $io->get_mem_usage() ne '', 'get_mem_usage() - Prints current memory usage to STDOUT. Need to auto flush the command line buffer');
+}
+
+$ENV{'PATH'} = '/bin/';
+
+diag( "Testing method execute_system_command()" );
+ok( $io->execute_system_command( "date", "Testing verbosity ... ", '1' ) , 'execute_system_command() - Is loadable and prints debug messages to STDOUT');
+
+diag( "Testing method execute_get_sys_cmd_output()" );
+ok( defined ($io->execute_get_sys_cmd_output( "date", "Testing execution of system level command: date ... ", '1' ) ) , 'execute_get_sys_cmd_output() - Is loadable and prints debug messages to STDOUT');
+
+diag( "Testing method open_file()" );
+if ( $ENV{'TMP'} ) {
+  diag( "Using user preferences and setting temporary location to $ENV{'TMP'}" );
+  $tmpLoc = $ENV{'TMP'}
+}
+elsif ( $ENV{'TEMP'} ) {
+  diag( "Using user preferences and setting temporary location to $ENV{'TEMP'}" );
+  $tmpLoc = $ENV{'TEMP'}
+}
+elsif ( $ENV{'TMPDIR'} ) {
+  diag( "Using user preferences and setting temporary location to $ENV{'TMPDIR'}" );
+  $tmpLoc = $ENV{'TMPDIR'}
+}
+else {
+  $tmpLoc = '/tmp';
+}
+
+$tmpLoc .= '/' if ( $tmpLoc !~ m/\/$/ );
+$tmpLoc .= 'IORoutineOpenFileTest.txt';
+
+ok( $io->open_file('>', $tmpLoc) ne '', 'open_file() - Returns non-empty filehandle for IO operations.');
+
+diag( "Testing method warning()" );
+ok( $io->warning( "Testing warnings ... " ) , 'warning() - Is loadable and prints warning messages to STDOUT');
+
+diag( "Testing method this_script_info()" );
+ok( $io->this_script_info('IO::Routine', "$IO::Routine::VERSION", 'Kranti Konganti', ' konganti', ' 2012-21-2012' ),
+    'this_script_info() - Prints the script banner to STDOUT');
+
+diag("\n\nTesting Complete!\n\n");
