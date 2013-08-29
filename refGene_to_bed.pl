f20b61e9e88112509c0264504e7ac288ec44e3cc
konganti <konganti@wsgi-hpc.tamu.edu>
2013-08-29 12:10:19 -0500

    adding refGene_to_bed.pl to github

---
+++ refGene_to_bed.pl
@@ -0,0 +1,115 @@
+#!/opt/perl/bin/perl
+
+# Change the shebang line above to point to your Perl installation
+
+use strict;
+use warnings;
+use Carp;
+use Getopt::Long;
+use IO::Routine;
+
+my $io = IO::Routine->new();
+
+my ($LASTCHANGEDBY) = q$LastChangedBy: konganti $ =~ m/.+?\:(.+)/;
+my ($LASTCHANGEDDATE) = q$LastChangedDate: 2012-12-01 11:16:31 -0600 (Sat, 01 Dec 2012) $ =~ m/.+?\:(.+)/;
+my ($VERSION) = q$LastChangedRevision: 45 $ =~ m/.+?(\d+)/;
+my $AUTHORFULLNAME = 'Kranti Konganti';
+
+my ($quiet, $refGeneTxt, $bedOut, $help);
+my $is_valid_option = GetOptions ('help|?'     => \$help,
+                                  'quiet'      => \$quiet,
+                                  'bed=s'      => \$bedOut,
+				  'refGene=s'  => \$refGeneTxt
+				 );
+
+$io->verify_options($is_valid_option);
+$io->verify_files([$refGeneTxt, $bedOut],
+		  ['refGene.txt', 'Path to Bed output file']);
+my $refGeneTxt_fh = $io->open_file('<', $refGeneTxt);
+my $bed_fh = $io->open_file('>', $bedOut);
+
+$io->this_script_info($0, $VERSION, $AUTHORFULLNAME, $LASTCHANGEDBY, $LASTCHANGEDDATE, $quiet);
+
+
+$io->execute_system_command(0,
+			   'Validating refGene.txt File ...',
+			   $quiet);
+
+while (my $line = <$refGeneTxt_fh>) {
+  $line = $io->strip_leading_and_trailing_spaces($line);
+  my @refGeneCols = split/\t/, $line;
+
+  $io->error("It seems like the $refGeneTxt file has invalid number of columns")
+    if (scalar(@refGeneCols) != 16);
+
+  print $bed_fh "$refGeneCols[2]\t$refGeneCols[4]\t$refGeneCols[5]\t$refGeneCols[12]\t$refGeneCols[11]\t$refGeneCols[3]\n";
+}
+
+close $bed_fh;
+close $refGeneTxt_fh;
+
+__END__
+
+=head1 NAME
+
+refGene_to_bed.pl
+
+=head1 SYNOPSIS
+
+This script will convert refGene.txt file to BED format.
+
+Examples:
+
+    perl refGene_to_bed.pl -h
+
+    perl refGene_to_bed.pl -r refGene.txt -b bedOutput.txt
+
+=head1 DESCRIPTION
+
+This script will use the following columns of the refGene table from UCSC to generate BED format.
+
+  chrom
+  strand
+  txStart
+  txEnd
+  score
+  name2
+
+=head1 OPTIONS
+
+refGene_to_bed.pl takes the following arguments:
+
+=over 4
+
+=item -h or --help (Optional)
+
+  Displays this helpful message.
+
+=item -q or --quiet (Optional)
+
+  Providing this option suppresses the log messages to the shell.
+  Default: disabled
+
+=item -r or --refGene (Required)
+
+  Path to the refGene.txt file you want to conver to BED.
+
+=item -b or --bed (Required)
+
+  Path to file to save the output.
+
+=back
+
+=head1 AUTHOR
+
+Kranti Konganti, E<lt>konganti@tamu.eduE<gt>.
+
+=head1 COPYRIGHT
+
+This program is distributed under the Artistic License.
+
+=head1 DATE
+
+Aug-29-2013
+
+=cut
