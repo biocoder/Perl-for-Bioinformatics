9429bb7071f0bfa383a37e7d5bac209ecdc5e832
biocoder <k.kranti.neu@gmail.com>
2014-07-23 18:12:46 -0500

    Making sure the dots are escaped in transcript ids

--- NGS-Utils/annotate_final_ncRNAs.sh
+++ NGS-Utils/annotate_final_ncRNAs.sh
@@ -13,10 +13,11 @@ if [  -z "$FINAL_GTF" ]  ||
     exit -1;
 fi
 
-grep noncoding $CPC_TXT_OUT | cut -d "|" -f 1 | sort -n | uniq | while read trid; do 
-    trlen=`grep "$trid" $FINAL_GTF | grep -oP 'transcript_length \"\d+\"' | head -n 1 | perl -e '\$line = <>; if (\$line =~ m/.+?(\d+)/) {print \$1;}'`;
-    hitlen=`grep "$trid" $CM_TXT_OUT | sort -k15,15nr | uniq | head -n 1 | awk '{if(\$10=="+") print \$9-\$8; else print \$8-\$9;}'`;
-    annot=`grep "$trid" $CM_TXT_OUT | sort -k15,15nr | uniq | awk '{$1=""; sc=$15; for(i=3;i<=17;i++) $i=""; if (length($0) != 0) print $0, " | BitScore: ",sc}' | cut -d " " -f 2- | sed -e 's/\s\+/ /g' | head -n 1`;
+grep noncoding $CPC_TXT_OUT | cut -d "|" -f 1 | sort -n | uniq | while read unetrid; do 
+    trid=${unetrid//\./\\.};
+    trlen=`grep -P "$trid" $FINAL_GTF | grep -oP 'transcript_length \"\d+\"' | head -n 1 | perl -e '\$line = <>; if (\$line =~ m/.+?(\d+)/) {print \$1;}'`;
+    hitlen=`grep -P "$trid" $CM_TXT_OUT | sort -k15,15nr | uniq | head -n 1 | awk '{if(\$10=="+") print \$9-\$8; else print \$8-\$9;}'`;
+    annot=`grep -P "$trid" $CM_TXT_OUT | sort -k15,15nr | uniq | awk '{$1=""; sc=$15; for(i=3;i<=17;i++) $i=""; if (length($0) != 0) print $0, " | BitScore: ",sc}' | cut -d " " -f 2- | sed -e 's/\s\+/ /g' | head -n 1`;
 
     if [ -z "$trlen" ] || [ -z "$hitlen" ]; then
 	calc_cov=0.0;
@@ -30,5 +31,5 @@ grep noncoding $CPC_TXT_OUT | cut -d "|" -f 1 | sort -n | uniq | while read trid
 	annot="$annot | Coverage: "$calc_cov$per_symb;
     fi 
 
-    grep "$trid" $FINAL_GTF | sed -e 's/Infernal.*//' | sed "s/$/ Infernal_prediction \"$annot\"\;/";
+    grep -P "$trid" $FINAL_GTF | sed -e 's/Infernal.*//' | sed "s/$/ Infernal_prediction \"$annot\"\;/";
 done;
